ff6f0e66e98425b3f4fca0de15a1372e750dc855
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-20 13:04:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/budget_db');
 
 // routes
 app.use(require("./routes/api.js"));
