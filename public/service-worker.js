a266eed47601e007b531c758567da7194b9d35b4
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-20 15:54:13 -0500

    Update service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version-01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+
+];
+
+// install event
+
+// activation event
+
+// fetch event 
