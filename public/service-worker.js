9771ac6dec6fe732e2a0628ee46c71203c0645d0
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-20 16:01:34 -0500

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,29 @@ const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version-01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-
+    "./css/styles.css",
+    "./icon/icon-72x72.png",
+    "./icon/icon-96x96.png",
+    "./icon/icon-128x128.png",
+    "./icon/icon-144x144.png",
+    "./icon/icon-152x152.png",
+    "./icon/icon-192x192.png",
+    "./icon/icon-384x384.png",
+    "./icon/icon-512x512.png",
+    "./js/idb.js",
+    "./js/index.js",
+    "./index.html"
 ];
 
 // install event
-
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log("installing chache: " + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    )
+})
 // activation event
 
 // fetch event 
