76fdb820f7213c01a0b7132b3ebbdac24fc38ed9
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-20 18:11:41 -0500

    Update idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,7 +3,7 @@ const request = indexedDB.open("budget_db", 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
-    db.createObjectStore('budget_store', { autoIncrement: true });
+    db.createObjectStore("budget_store", { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
@@ -18,11 +18,37 @@ request.onerror = function(event) {
 };
 
 function saveRecord(record) {
-
+    const trans = db.trans(["budget_store"], "readwrite");
+    const bgtObjectStore = trans.objectStore("budget_store");
+    bgtObjectStore.add(record);
 };
 
 function uploadNewBudget(){
-
+    const trans = db.trans(["budget_store"], "readwrite");
+    const bgtObjectStore = trans.objectStore("budget_store");
+    const getAll = bgtObjectStore.getAll();
+    getAll.onsuccess = function(){
+        if(getAll.result.length > 0) {
+            fetch("api/transaction", {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(res => res.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const trans = db.trans(["budget_store"], 'readwrite');
+                const bgtObjectStore = trans.objectStore("budget_store");
+                bgtObjectStore.clear();
+            })
+            .catch(err => console.log(err));
+        };
+    }; 
 };
 
 window.addEventListener("online", uploadNewBudget);
